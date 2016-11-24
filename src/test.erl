1aeea9ce67ef7749c2e7c6d8c06bf746fb144ef0
Fang-Li <649505671@qq.com>
2016-11-24 10:59:38 +0800

    Create test.erl

---
+++ src/test.erl
@@ -0,0 +1,19 @@
+-module(test).
+-compile(export_all).
+-define(DBGFSM, p1_fsm).
+-ifdef(DBGFSM).
+-define(FSMOPTS, [{debug, [trace]}]).
+-else.
+-define(FSMOPTS, []).
+-endif.
+
+test() ->
+  io:format("~p~n",[?FSMOPTS]).
+  
+io() ->
+
+    io:format("~ts~n", ["中国"]),
+    % io:format("~ts~n", [list_to_binary("中国")]),
+
+  io:format("~ts~n", [[228,184,173,229,155,189]]),
+  io:format("~ts~n", [<<228,184,173,229,155,189>>]).
