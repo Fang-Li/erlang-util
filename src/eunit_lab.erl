05770626625bcac03187d1584920d0f0a4b174e0
lifang <649505671@qq.com>
2019-08-22 22:50:58 +0800

    单元测试错误使用规范

--- src/eunit_lab.erl
+++ src/eunit_lab.erl
@@ -10,46 +10,58 @@
 -author("lifang").
 
 -export([]).
+-compile(export_all).
 %%-define(NOTEST, 1).
 -include_lib("eunit/include/eunit.hrl").
 
 %% 简单测试
-reverse_test() -> lists:reverse([1,2,3]).
+reverse_test() -> lists:reverse([1, 2, 3]).
 
 %% 加入验证
 reverse_nil_test() -> [] = lists:reverse([]).
 reverse_one_test() -> [1] = lists:reverse([1]).
-reverse_two_test() -> [2,1] = lists:reverse([1,2]).
+reverse_two_test() -> [2, 1] = lists:reverse([1, 2]).
 
 
 %% 优雅验证
-length_test() -> ?assert(length([1,2,3]) =:= 3).
+length_test() -> ?assert(length([1, 2, 3]) =:= 3).
 
 %% 生成函数
 basic_test_() ->
-  fun () -> ?assert(1 + 1 =:= 2) end.
+    fun() -> ?assert(1 + 1 =:= 2) end.
 
 
 %% 格式紧凑点
 basic2_test_() ->
-  ?_test(?assert(1 + 1 =:= 2)).
+    ?_test(?assert(1 + 1 =:= 2)).
 
 basic3_test_() ->
-  ?_assert(1 + 1 =:= 2).
+    ?_assert(1 + 1 =:= 2).
 
 
 %% 有了生成函数, 不会执行普通函数了
 
 add_test_() ->
-  [test_them_types(),
-    test_them_values(),
-    ?_assertError(badarith, 1/0)].
+    [test_them_types(),
+        test_them_values(),
+        ?_assertError(badarith, 1 / 0)].
 
 test_them_types() ->
-  ?_assert(is_number(ops:add(1,2))).
+    ?_assert(is_number(ops:add(1, 2))).
+
+value_test() ->
+    ?assertEqual(4, add(1, 2)),
+    ?assertEqual(4, add(1, 2)),
+    ?assertEqual(4, add(1, 2)),
+    ?assertEqual(4, add(1, 2)),
+    ?assertEqual(4, add(1, 2)).
 
 test_them_values() ->
-  [?_assertEqual(4, ops:add(2,2)),
-    ?_assertEqual(3, ops:add(1,2)),
-    ?_assertEqual(3, ops:add(1,1))].
+    [?_assertEqual(4, add(2, 2)),
+        ?_assertEqual(3, add(1, 2)),
+        ?_assertEqual(3, add(1, 1)),
+        ?_assertEqual(3, add(1, 1)),
+        ?_assertEqual(3, add(1, 1))].
 
+add(A, B) ->
+    A + B.
