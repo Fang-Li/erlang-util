630daeaa714182cb358796622c75611109179123
lifang <649505671@qq.com>
2020-01-19 21:00:03 +0800

    aa

--- src/node.erl
+++ src/node.erl
@@ -1,18 +1,18 @@
 -module(node).
 -compile(export_all).
 
-remote(OtherNode)->
+remote(OtherNode) ->
 % get the PID of the user server on OtherNode
 % OtherNode = 'bb@10.104.101.246'.
-RemoteUser = rpc:call(OtherNode, erlang,whereis,[user]), 
+    RemoteUser = rpc:call(OtherNode, erlang, whereis, [user]),
 
-true = is_pid(RemoteUser),
+    true = is_pid(RemoteUser),
 
 % send message to remote PID
-RemoteUser ! ignore_this, 
+    RemoteUser ! ignore_this,
 
 % print "Hello from <nodename>\n" on the remote node's console.
-io:format(RemoteUser, "Hello from ~p~n", [node()]). 
+    io:format(RemoteUser, "Hello from ~p~n", [node()]).
 
 % Module:
 % user
